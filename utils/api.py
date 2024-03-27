d099d70db5f70804debee1ee4afb155144cdd721
Cano <cano@list.ru>
2024-03-27 23:52:20 +0300

    patch

--- utils/api.py
+++ utils/api.py
@@ -29,3 +29,29 @@ class Get:
         response = self.client.custom_request("GET", f"{self.url}{prefix}")
         logger.info(prefix)
         return response
+
+
+class Patch:
+    def __init__(self, url):
+        self.settings = settings
+        self.url = url
+        self.client = Client()
+
+    def patch(self, body: dict, schema: dict):
+        response = self.client.custom_request("PATCH", f"{self.url}{settings.PATCH}", json=body)
+        validate(instance=response.json(), schema=schema)
+        logger.info(response.status_code)
+        logger.info(response.text)
+        return response
+
+    def put(self, body: dict, schema: dict):
+        response = self.client.custom_request("PUT", f"{self.url}{settings.PATCH}", json=body)
+        validate(instance=response.json(), schema=schema)
+        logger.info("A PUT request recieved %s code", response.status_code)
+        logger.info(response.text)
+        return response
+
+    def delete(self):
+        response = self.client.custom_request("DELETE", f"{self.url}{settings.DELETE}")
+        logger.info("A DELETE request recieved %s code", response.status_code)
+        return response
